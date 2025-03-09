7aaf1eb57c15871618d2ab2fb45fd3ffc1219d47
Bohdan Zakovinko <b.zakovinko@gmail.com>
2025-03-09 12:57:30 +0400

    i feel pain

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,4 +1,5 @@
 import { useState, useRef, useEffect } from "react";
+import "./search-animation.css";
 
 export function SearchForm() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -23,36 +24,22 @@ export function SearchForm() {
   };
 
   return (
-    <form className="group m-2 flex w-full" onSubmit={handleSubmit}>
+    <form
+      className="animated-search-container m-2 w-full"
+      onSubmit={handleSubmit}
+    >
+      <label htmlFor="search">Search</label>
       <input
         ref={inputRef}
+        id="search"
+        type="search"
+        pattern=".*\S.*"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        type="text"
-        className="flex-1 rounded-l-md border-2 border-r-0 border-teal-800/90 bg-teal-950/50 p-3 text-teal-50 group-hover:border-teal-900/90 group-active:border-teal-950/90 placeholder:italic hover:text-teal-100/90 focus-visible:outline-hidden active:text-teal-200/90"
+        required
         placeholder="Search for everything and more"
       />
-
-      <button
-        type="submit"
-        className="cursor-pointer rounded-r-md bg-teal-800/90 px-4 py-2 text-lg text-teal-50 font-stretch-condensed transition-colors group-hover:bg-teal-900/90 group-active:bg-teal-950/90 hover:text-teal-200/90 active:text-teal-200/90"
-        aria-label="Search"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-      </button>
+      <span className="caret"></span>
     </form>
   );
 }
